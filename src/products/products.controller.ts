c58c34dba1221c87fefd1e72317eb6572f7cdc83
Renier Vargas <renier1989@gmail.com>
2024-08-18 18:57:52 -0500

    manejo simple de expciones

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,43 +1,56 @@
-import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 import { PaginatinoDto } from 'src/common';
 import { PRODUCT_SERVICE } from 'src/config';
 
 @Controller('products')
 export class ProductsController {
-
   constructor(
-  @Inject(PRODUCT_SERVICE) private readonly productClient: ClientProxy
+    @Inject(PRODUCT_SERVICE) private readonly productClient: ClientProxy,
   ) {}
 
-
   @Post()
-  createProdcut(){
+  createProdcut() {
     return 'Crear un producto';
   }
 
   @Get()
-  findAllProducts(@Query() paginatinoDto: PaginatinoDto){
-    return this.productClient.send({cmd: 'find_all'}, paginatinoDto)
+  findAllProducts(@Query() paginatinoDto: PaginatinoDto) {
+    return this.productClient.send({ cmd: 'find_all' }, paginatinoDto);
     // return this.productClient.send({cmd: 'find_all'}, { limit : 2 , page:2 })
   }
 
   @Get(':id')
-  findOneProduct(@Param('id') id:string){
-    return `Regresa el producto : ${id}`;
+  async findOneProduct(@Param('id') id: string) {
+    try {
+      const product = await firstValueFrom(
+        this.productClient.send({ cmd: 'find_one_product' }, { id: id }),
+      );
+      return product;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 
   @Delete(':id')
-  deleteProduct(@Param('id') id:string){
+  deleteProduct(@Param('id') id: string) {
     return `Elimina el producto : ${id}`;
   }
 
   @Patch(':id')
-  patchProduct(
-    @Param('id') id:string,  
-    @Body() body:any
-  ){
+  patchProduct(@Param('id') id: string, @Body() body: any) {
     return `Actualiza el producto: ${id}`;
   }
-
 }
